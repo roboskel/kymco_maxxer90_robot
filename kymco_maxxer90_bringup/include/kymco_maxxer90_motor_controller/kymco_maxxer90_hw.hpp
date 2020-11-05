4bbcd2cdb10e9c6aa0dbb1408261ba96b01f043f
George Stavrinos <stavrinosgeo@gmail.com>
2020-11-05 15:46:57 +0200

    Better naming schemes

--- kymco_maxxer90_bringup/include/kymco_maxxer90_motor_controller/kymco_maxxer90_hw.hpp
+++ kymco_maxxer90_bringup/include/kymco_maxxer90_motor_controller/kymco_maxxer90_hw.hpp
@@ -12,26 +12,29 @@
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/joint_command_interface.h>
 
-namespace kymco_maxxer90_motor_controller {
+namespace kymco_maxxer90_ackermann_steering_controller {
 
 static const std::string THROTTLE_START = "$SPEED,";
-static const std::string THROTTLE_END = "*1\r\n";
+static const std::string THROTTLE_END = "*11\r\n";
 static const std::string STEERING_START = "$ANGLE,";
-static const std::string STEERING_END = "*1\r\n";
+static const std::string STEERING_END = "*11\r\n";
 static const std::string THROTTLE_JOINT = "RWD_joint";
 static const std::string STEERING_JOINT = "steering_wheel_joint";
-static const double RAD_PER_SEC = 50*M_PI/180/2;
-static const double DEG_PER_SEC = 50/2;
 static const int  MIN_VELX = 0;
 static const int  MAX_VELX = 9;
 static const int MIN_ANGZ = 0;
 static const int MAX_ANGZ = 49;
+static const double WHEEL_DIAMETER = 0.4572; //m
+static const double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * M_PI;
+static const double METERS_PER_ENCODER_TICK = 1.f/39.f; // 39 ticks per meter
+static const double RAD_PER_SEC = (MAX_ANGZ-MIN_ANGZ+1)*M_PI/180.f/2.f;
+static const double DEG_PER_SEC = (MAX_ANGZ-MIN_ANGZ+1)/2.f;
 
-class KymcoMaxxer90MotorController : public hardware_interface::RobotHW {
+class KymcoMaxxer90AckermannSteeringController : public hardware_interface::RobotHW {
 
     public:
-        KymcoMaxxer90MotorController(const ros::NodeHandle&, const std::string& = "/dev/ttyUSB1", const std::string& = "/dev/ttyUSB2", const std::string& = "/dev/ttyUSB0", const int& = 4800, const int& = 4800, const int& = 19200, const std::string& = "motor_driver", const std::string& = "motor_driver", const std::string& = "arduino");
-        ~KymcoMaxxer90MotorController();
+        KymcoMaxxer90AckermannSteeringController(const ros::NodeHandle&, const std::string& = "/dev/ttyUSB1", const std::string& = "/dev/ttyUSB2", const std::string& = "/dev/ttyUSB0", const int& = 4800, const int& = 4800, const int& = 19200, const std::string& = "motor_driver", const std::string& = "motor_driver", const std::string& = "arduino");
+        ~KymcoMaxxer90AckermannSteeringController();
 
     private:
         void serialClose();
@@ -47,20 +50,19 @@ class KymcoMaxxer90MotorController : public hardware_interface::RobotHW {
         void connectToMotorDriver(const std::string&, const std::string&, const int&, const int&, const std::string&, const std::string&);
 
         ros::NodeHandle nh;
-        ros::Publisher odom_pub;
-        int target_steering_angle;
-        double curr_steering_angle;
-        ros::Duration elapsed_time;
-        geometry_msgs::Twist cmd_vel;
+        ros::Subscriber spray_sub;
+        ros::Time last_encoder_reading;
         sensor_msgs::JointState state;
         serial::Serial *srl1, *srl2, *srl3;
-        ros::Subscriber cmd_vel_sub, spray_sub;
         ros::Timer update_timer, encoders_timer;
+        ros::Duration elapsed_time, elapsed_time2;
+        int target_steering_angle, encoder_reading;
         hardware_interface::JointStateInterface jstate_interface;
         hardware_interface::VelocityJointInterface jvel_interface;
         hardware_interface::PositionJointInterface jpos_interface;
         hardware_interface::EffortJointInterface jeff_interface;
         std::shared_ptr<controller_manager::ControllerManager> controller_manager;
+        double curr_steering_angle, linear_speed, angular_speed, linear_velocity, angular_velocity;
 
 };
 
--- kymco_maxxer90_bringup/src/motor_controller.cpp
+++ kymco_maxxer90_bringup/src/motor_controller.cpp
@@ -1,22 +1,22 @@
 #include <kymco_maxxer90_motor_controller/kymco_maxxer90_hw.hpp>
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "kymco_maxxer90_motor_controller");
+    ros::init(argc, argv, "kymco_maxxer90_ackermann_steering_controller");
     ros::NodeHandle nh;
 
     std::string port1, port2, port3;
     int baudrate1, baudrate2, baudrate3;
 
-    nh.param("kymco_maxxer90_motor_controller/port1", port1, std::string("/dev/ttyUSB0"));
-    nh.param("kymco_maxxer90_motor_controller/port2", port2, std::string("/dev/ttyUSB1"));
-    nh.param("kymco_maxxer90_motor_controller/port3", port3, std::string("/dev/ttyACM0"));
-    nh.param("kymco_maxxer90_motor_controller/baudrate1", baudrate1, 4800);
-    nh.param("kymco_maxxer90_motor_controller/baudrate2", baudrate2, 4800);
-    nh.param("kymco_maxxer90_motor_controller/baudrate3", baudrate3, 19200);
+    nh.param("kymco_maxxer90_ackermann_steering_controller/port1", port1, std::string("/dev/ttyUSB1"));
+    nh.param("kymco_maxxer90_ackermann_steering_controller/port2", port2, std::string("/dev/ttyUSB2"));
+    nh.param("kymco_maxxer90_ackermann_steering_controller/port3", port3, std::string("/dev/ttyUSB0"));
+    nh.param("kymco_maxxer90_ackermann_steering_controller/baudrate1", baudrate1, 4800);
+    nh.param("kymco_maxxer90_ackermann_steering_controller/baudrate2", baudrate2, 4800);
+    nh.param("kymco_maxxer90_ackermann_steering_controller/baudrate3", baudrate3, 19200);
 
     ros::MultiThreadedSpinner spinner(3);
 
-    kymco_maxxer90_motor_controller::KymcoMaxxer90MotorController km90(nh);
+    kymco_maxxer90_ackermann_steering_controller::KymcoMaxxer90AckermannSteeringController km90(nh);
 
     spinner.spin();
 
