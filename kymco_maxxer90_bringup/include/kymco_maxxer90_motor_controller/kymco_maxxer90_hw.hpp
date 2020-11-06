98565228c08fec7bb699cf23ca09a3ebad9a1d35
George Stavrinos <stavrinosgeo@gmail.com>
2020-11-06 17:04:41 +0200

    First complete driver implementation. It includes all required joint states for a complete tf tree and odometry. It requires configuration based on the real hardware.

--- kymco_maxxer90_bringup/include/kymco_maxxer90_motor_controller/kymco_maxxer90_hw.hpp
+++ kymco_maxxer90_bringup/include/kymco_maxxer90_motor_controller/kymco_maxxer90_hw.hpp
@@ -20,15 +20,26 @@ static const std::string STEERING_START = "$ANGLE,";
 static const std::string STEERING_END = "*11\r\n";
 static const std::string THROTTLE_JOINT = "RWD_joint";
 static const std::string STEERING_JOINT = "steering_wheel_joint";
+static const std::string FRONT_LEFT_WHEEL_JOINT = "front_left_wheel";
+static const std::string FRONT_RIGHT_WHEEL_JOINT = "front_right_wheel";
+static const std::string REAR_LEFT_WHEEL_JOINT = "rear_left_wheel";
+static const std::string REAR_RIGHT_WHEEL_JOINT = "rear_right_wheel";
+static const std::string REAR_SUSPENSION_JOINT = "rear_suspension_joint";
+static const std::string FRONT_LEFT_SUSPENSION_JOINT = "front_left_suspension_joint";
+static const std::string FRONT_RIGHT_SUSPENSION_JOINT = "front_right_suspension_joint";
+static const std::string FRONT_LEFT_STEERING_JOINT = "front_left_steering_wheel_joint";
+static const std::string FRONT_RIGHT_STEERING_JOINT = "front_right_steering_wheel_joint";
+
 static const int  MIN_VELX = 0;
 static const int  MAX_VELX = 9;
 static const int MIN_ANGZ = 0;
 static const int MAX_ANGZ = 49;
 static const double WHEEL_DIAMETER = 0.4572; //m
-static const double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * M_PI;
 static const double METERS_PER_ENCODER_TICK = 1.f/39.f; // 39 ticks per meter
-static const double RAD_PER_SEC = (MAX_ANGZ-MIN_ANGZ+1)*M_PI/180.f/2.f;
 static const double DEG_PER_SEC = (MAX_ANGZ-MIN_ANGZ+1)/2.f;
+static const double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * M_PI;
+static const double RAD_PER_SEC = DEG_PER_SEC*M_PI/180.f;
+static const double CENTERED_RAD_STEERING = int((MAX_ANGZ - MIN_ANGZ) / 2.f) * M_PI / 180.f;
 
 class KymcoMaxxer90AckermannSteeringController : public hardware_interface::RobotHW {
 
@@ -37,32 +48,32 @@ class KymcoMaxxer90AckermannSteeringController : public hardware_interface::Robo
         ~KymcoMaxxer90AckermannSteeringController();
 
     private:
+        void control();
         void serialClose();
         void centerSteeringWheel();
+        void writeThrottleSerial();
+        void writeSteeringSerial();
         void update(const ros::TimerEvent&);
-        void writeThrottleSerial(const double&);
-        void writeSteeringSerial(const double&);
         void readEncoders(const ros::TimerEvent&);
         void sprayingCallback(const std_msgs::Bool::ConstPtr&);
-        void cmdVelCallback(const geometry_msgs::Twist::ConstPtr&);
-        int norm(const double&, const double&, const double&, const double&, const double&);
+        double norm(const double&, const double&, const double&, const double&, const double&);
         void serialInit(serial::Serial*&, const std::string&, const int&, const std::string&);
         void connectToMotorDriver(const std::string&, const std::string&, const int&, const int&, const std::string&, const std::string&);
 
         ros::NodeHandle nh;
+        int encoder_reading;
         ros::Subscriber spray_sub;
-        ros::Time last_encoder_reading;
         sensor_msgs::JointState state;
+        ros::Time last_encoder_reading;
         serial::Serial *srl1, *srl2, *srl3;
         ros::Timer update_timer, encoders_timer;
         ros::Duration elapsed_time, elapsed_time2;
-        int target_steering_angle, encoder_reading;
+        hardware_interface::EffortJointInterface jeff_interface;
         hardware_interface::JointStateInterface jstate_interface;
         hardware_interface::VelocityJointInterface jvel_interface;
         hardware_interface::PositionJointInterface jpos_interface;
-        hardware_interface::EffortJointInterface jeff_interface;
         std::shared_ptr<controller_manager::ControllerManager> controller_manager;
-        double curr_steering_angle, linear_speed, angular_speed, linear_velocity, angular_velocity;
+        double curr_steering_angle, target_steering_angle, linear_velocity, angular_velocity;
 
 };
 
--- kymco_maxxer90_bringup/src/kymco_maxxer90_hw.cpp
+++ kymco_maxxer90_bringup/src/kymco_maxxer90_hw.cpp
@@ -4,51 +4,66 @@ using namespace kymco_maxxer90_ackermann_steering_controller;
 
 KymcoMaxxer90AckermannSteeringController::KymcoMaxxer90AckermannSteeringController(const ros::NodeHandle& n, const std::string& port1, const std::string& port2, const std::string& port3, const int& baudrate1, const int& baudrate2, const int& baudrate3, const std::string& device_name1, const std::string& device_name2, const std::string& device_name3) {
 
+    // NOTE:
+    // srl1 -> Steering
+    // srl2 -> Throttle
+    // srl3 -> Arduino (spraying, encoders, etc)
+
     connectToMotorDriver(port1, port2, baudrate1, baudrate2, device_name1, device_name2);
-    // serialInit(srl3, port3, baudrate3, device_name3);
+    serialInit(srl3, port3, baudrate3, device_name3);
 
     nh = n;
 
-    state.position.push_back(0.0);
-    state.position.push_back(0.0);
-    state.velocity.push_back(0.0);
-    state.velocity.push_back(0.0);
-    state.effort.push_back(0.0);
-    state.effort.push_back(0.0);
     state.name.push_back(THROTTLE_JOINT);
     state.name.push_back(STEERING_JOINT);
+    state.name.push_back(FRONT_RIGHT_WHEEL_JOINT);
+    state.name.push_back(FRONT_LEFT_WHEEL_JOINT);
+    state.name.push_back(REAR_RIGHT_WHEEL_JOINT);
+    state.name.push_back(REAR_LEFT_WHEEL_JOINT);
+    state.name.push_back(FRONT_RIGHT_STEERING_JOINT);
+    state.name.push_back(FRONT_LEFT_STEERING_JOINT);
+    state.name.push_back(FRONT_RIGHT_SUSPENSION_JOINT);
+    state.name.push_back(FRONT_LEFT_SUSPENSION_JOINT);
+    state.name.push_back(REAR_SUSPENSION_JOINT);
+    for(unsigned i=0; i<11; i++) {
+        state.position.push_back(0.0);
+        state.velocity.push_back(0.0);
+        state.effort.push_back(0.0);
+    }
+    // Handlers do not work when initialised inside the for loop
+    // and I am too lazy to create a struct/class
+    hardware_interface::JointStateHandle state_handler0(state.name.at(0), &state.position[0], &state.velocity[0], &state.effort[0]);
+    jstate_interface.registerHandle(state_handler0);
+    hardware_interface::JointStateHandle state_handler1(state.name.at(1), &state.position[1], &state.velocity[1], &state.effort[1]);
+    jstate_interface.registerHandle(state_handler1);
+    hardware_interface::JointStateHandle state_handler2(state.name.at(2), &state.position[2], &state.velocity[2], &state.effort[2]);
+    jstate_interface.registerHandle(state_handler2);
+    hardware_interface::JointStateHandle state_handler3(state.name.at(3), &state.position[3], &state.velocity[3], &state.effort[3]);
+    jstate_interface.registerHandle(state_handler3);
+    hardware_interface::JointStateHandle state_handler4(state.name.at(4), &state.position[4], &state.velocity[4], &state.effort[4]);
+    jstate_interface.registerHandle(state_handler4);
+    hardware_interface::JointStateHandle state_handler5(state.name.at(5), &state.position[5], &state.velocity[5], &state.effort[5]);
+    jstate_interface.registerHandle(state_handler5);
+    hardware_interface::JointStateHandle state_handler6(state.name.at(6), &state.position[6], &state.velocity[6], &state.effort[6]);
+    jstate_interface.registerHandle(state_handler6);
+    hardware_interface::JointStateHandle state_handler7(state.name.at(7), &state.position[7], &state.velocity[7], &state.effort[7]);
+    jstate_interface.registerHandle(state_handler7);
+    hardware_interface::JointStateHandle state_handler8(state.name.at(8), &state.position[8], &state.velocity[8], &state.effort[8]);
+    jstate_interface.registerHandle(state_handler8);
+    hardware_interface::JointStateHandle state_handler9(state.name.at(9), &state.position[9], &state.velocity[9], &state.effort[9]);
+    jstate_interface.registerHandle(state_handler9);
+    hardware_interface::JointStateHandle state_handler10(state.name.at(10), &state.position[10], &state.velocity[10], &state.effort[10]);
+    jstate_interface.registerHandle(state_handler10);
 
-    curr_steering_angle = 25;
-    target_steering_angle = 25.0;
-
-    hardware_interface::JointStateHandle state_linear_x(THROTTLE_JOINT, &state.position[0], &state.velocity[0], &state.effort[0]);
-    hardware_interface::JointStateHandle state_angular_z(STEERING_JOINT, &state.position[1], &state.velocity[1], &state.effort[1]);
-    jstate_interface.registerHandle(state_linear_x);
-    jstate_interface.registerHandle(state_angular_z);
-
-    // hardware_interface::JointHandle vel_handle_linx(jstate_interface.getHandle(THROTTLE_JOINT), &state.velocity[0]);
-    // hardware_interface::JointHandle vel_handle_angz(jstate_interface.getHandle(STEERING_JOINT), &state.velocity[1]);
-    // jvel_interface.registerHandle(vel_handle_linx);
-    // jvel_interface.registerHandle(vel_handle_angz);
     hardware_interface::JointHandle vel_handle_linx(jstate_interface.getHandle(THROTTLE_JOINT), &linear_velocity);
-    // hardware_interface::JointHandle vel_handle_angz(jstate_interface.getHandle(STEERING_JOINT), &angular_velocity);
     jvel_interface.registerHandle(vel_handle_linx);
-    // jvel_interface.registerHandle(vel_handle_angz);
 
-    // hardware_interface::JointHandle pos_handle_linx(jstate_interface.getHandle(THROTTLE_JOINT), &state.position[0]);
     hardware_interface::JointHandle pos_handle_angz(jstate_interface.getHandle(STEERING_JOINT), &angular_velocity);
-    // jpos_interface.registerHandle(pos_handle_linx);
     jpos_interface.registerHandle(pos_handle_angz);
-
-    // hardware_interface::JointHandle eff_handle_linx(jstate_interface.getHandle(THROTTLE_JOINT), &state.effort[0]);
-    // hardware_interface::JointHandle eff_handle_angz(jstate_interface.getHandle(STEERING_JOINT), &state.effort[1]);
-    // jeff_interface.registerHandle(eff_handle_linx);
-    // jeff_interface.registerHandle(eff_handle_angz);
 
     registerInterface(&jstate_interface);
     registerInterface(&jvel_interface);
     registerInterface(&jpos_interface);
-    // registerInterface(&jeff_interface);
 
     controller_manager.reset(new controller_manager::ControllerManager(this, nh));
 
@@ -65,34 +80,51 @@ KymcoMaxxer90AckermannSteeringController::~KymcoMaxxer90AckermannSteeringControl
 }
 
 void KymcoMaxxer90AckermannSteeringController::update(const ros::TimerEvent& e) {
+    target_steering_angle = norm(angular_velocity, 1.0, -1.0, MIN_ANGZ, MAX_ANGZ);
     elapsed_time = ros::Duration(e.current_real - e.last_real);
 
     double delta_steering = std::min(abs(target_steering_angle - curr_steering_angle), elapsed_time.toSec() * DEG_PER_SEC);
     curr_steering_angle = target_steering_angle > curr_steering_angle ? curr_steering_angle + delta_steering : curr_steering_angle - delta_steering;
-    double centered_rad_steering = (MAX_ANGZ - MIN_ANGZ) / 2.f * M_PI / 180.f;
 
-    double new_steering_pos = norm(curr_steering_angle, MIN_ANGZ, MAX_ANGZ, -centered_rad_steering, centered_rad_steering);
+    double new_steering_pos = norm(curr_steering_angle, MIN_ANGZ, MAX_ANGZ, -CENTERED_RAD_STEERING, CENTERED_RAD_STEERING);
 
     double dx = encoder_reading * METERS_PER_ENCODER_TICK; // m
 
-    double new_throttle_pos = (2 * M_PI) / (dx / WHEEL_CIRCUMFERENCE); // rad
+    // double new_throttle_pos = dx != 0 ? (2 * M_PI) / (dx / WHEEL_CIRCUMFERENCE) : 0; // rad
 
     // Throttle joints
-    state.velocity[0] = new_throttle_pos / elapsed_time2.toSec(); // rad/s;
-    state.position[0] += new_throttle_pos;
+    // state.velocity[0] = elapsed_time2.toSec() != 0 ? new_throttle_pos / elapsed_time2.toSec() : 0; // rad/s;
+    // state.position[0] += new_throttle_pos; //rad
+
+    // NOTE: Even though joints are documented to be measured in rad and rad/s for position and velocity
+    // respectively, ackermann_steering_controller expects m and m/s (!)
+    state.velocity[0] = elapsed_time2.toSec() != 0 ? dx / elapsed_time2.toSec() : 0; // m/s;
+    state.position[0] += dx; // m
+    // All wheels blindly mimic the RWD joint
+    state.position[2] = -state.position[0];
+    state.velocity[2] = -state.velocity[0];
+    state.position[3] = state.position[0];
+    state.velocity[3] = state.velocity[0];
+    state.position[4] = -state.position[0];
+    state.velocity[4] = -state.velocity[0];
+    state.position[5] = state.position[0];
+    state.velocity[5] = state.velocity[0];
 
     // Steering joints
     state.velocity[1] = (new_steering_pos - state.position[1]) / elapsed_time.toSec();
     state.position[1] = new_steering_pos;
+    // Front wheels blindly mimic the steering wheel
+    state.position[6] = state.position[1];
+    state.position[7] = state.position[1];
 
     controller_manager->update(ros::Time::now(), elapsed_time);
 
+    control();
 }
 
 void KymcoMaxxer90AckermannSteeringController::readEncoders(const ros::TimerEvent& e) {
     try {
-        // std::string response = srl3->read(9);
-        std::string response = "10";
+        std::string response = srl3->read(9);
         if (not response.empty()) {
             encoder_reading = std::stoi(response);
             ros::Time t = ros::Time::now();
@@ -107,22 +139,22 @@ void KymcoMaxxer90AckermannSteeringController::sprayingCallback(const std_msgs::
     srl3->write("CV"+std::to_string(int(msg->data)));
 }
 
-void KymcoMaxxer90AckermannSteeringController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr& cmd_vel) {
-    writeThrottleSerial(cmd_vel->linear.x);
-    writeSteeringSerial(cmd_vel->angular.z);
+void KymcoMaxxer90AckermannSteeringController::control() {
+    writeThrottleSerial();
+    writeSteeringSerial();
 }
 
 void KymcoMaxxer90AckermannSteeringController::connectToMotorDriver(const std::string& port1, const std::string& port2, const int& baudrate1, const int& baudrate2, const std::string& device_name1, const std::string& device_name2) {
 
-    // serialInit(srl1, port1, baudrate1, device_name1);
-    // serialInit(srl2, port2, baudrate2, device_name2);
+    serialInit(srl1, port1, baudrate1, device_name1);
+    serialInit(srl2, port2, baudrate2, device_name2);
 
     centerSteeringWheel();
-    // std::string response = srl1->read(9);
-    // if (response.find("$ANG") >= response.length()) {
-        // std::swap(srl1,srl2);
-        // centerSteeringWheel();
-    // }
+    std::string response = srl1->read(9);
+    if (response.find("$ANG") >= response.length()) {
+        std::swap(srl1,srl2);
+        centerSteeringWheel();
+    }
 
 }
 
@@ -145,7 +177,7 @@ void KymcoMaxxer90AckermannSteeringController::serialInit(serial::Serial *&srl, 
 }
 
 void KymcoMaxxer90AckermannSteeringController::centerSteeringWheel() {
-    // srl1->write(STEERING_START + std::to_string(int((MAX_ANGZ-MIN_ANGZ)/2)) + STEERING_END);
+    srl1->write(STEERING_START + std::to_string(int((MAX_ANGZ-MIN_ANGZ)/2)) + STEERING_END);
 }
 
 void KymcoMaxxer90AckermannSteeringController::serialClose() {
@@ -160,22 +192,21 @@ void KymcoMaxxer90AckermannSteeringController::serialClose() {
     }
 }
 
-void KymcoMaxxer90AckermannSteeringController::writeThrottleSerial(const double& x) {
-    if (x >= 0) {
-        int v = norm(x, MIN_VELX, MAX_VELX, 0, 1.0);
-        // srl2->write(THROTTLE_START + std::to_string(v) + THROTTLE_END);
+void KymcoMaxxer90AckermannSteeringController::writeThrottleSerial() {
+    if (linear_velocity >= 0) {
+        int v = norm(linear_velocity, MIN_VELX, MAX_VELX, 0, 1.0);
+        srl2->write(THROTTLE_START + std::to_string(v) + THROTTLE_END);
     }
     else {
         ROS_WARN("Received negative X velocity value. Our robot cannot move backwards. Ignoring.");
     }
 }
 
-void KymcoMaxxer90AckermannSteeringController::writeSteeringSerial(const double& z) {
-    target_steering_angle = norm(z, 1.0, -1.0, MIN_ANGZ, MAX_ANGZ);
-    // srl1->write(STEERING_START + std::to_string(target_steering_angle) + STEERING_END);
+void KymcoMaxxer90AckermannSteeringController::writeSteeringSerial() {
+    srl1->write(STEERING_START + std::to_string((int)target_steering_angle) + STEERING_END);
 }
 
-int KymcoMaxxer90AckermannSteeringController::norm(const double& v1, const double& s1, const double& e1, const double& s2, const double& e2) {
+double KymcoMaxxer90AckermannSteeringController::norm(const double& v1, const double& s1, const double& e1, const double& s2, const double& e2) {
     return (((v1 - s1) * (e2 - s2)) / (e1 - s1)) + s2;
 }
 
