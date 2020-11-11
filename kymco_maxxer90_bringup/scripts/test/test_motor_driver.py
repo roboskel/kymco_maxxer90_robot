05c1a06fda087123bbabba35cea6eef080bb9d82
George Stavrinos <stavrinosgeo@gmail.com>
2020-11-11 17:46:58 +0200

    Created a more thorough test for the motor drivers

--- kymco_maxxer90_bringup/scripts/test/test_motor_driver.py
+++ kymco_maxxer90_bringup/scripts/test/test_motor_driver.py
@@ -12,18 +12,26 @@ print("---------------")
 s2 = serial.Serial("/dev/ttyUSB1", 4800)
 s1 = serial.Serial("/dev/ttyUSB2", 4800)
 
-throttle_cmd = "$SPEED,1*1\r\n"
-steering_cmd = "$ANGLE,25*1\r\n"
+start_s = "$ANGLE,"
+steering_cmds = [0,25,49,25]
+end_s = "*11\r\n"
+start_t = "$SPEED,"
+throttle_cmds = [0,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
+end_t = "*11\r\n"
 
 # Assuming s1 is the throttle board
 # and s2 is the steering board
 
 while(True):
-    print("--SENDING THROTTLE COMMAND--")
-    s1.write(throttle_cmd)
-    print("--SENDING STEERING COMMAND--")
-    s2.write(steering_cmd)
-
-    print("----------- DONE -----------")
-    print(s1.readline().strip())
-    time.sleep(2)
+    for i in throttle_cmds:
+        print("--SENDING THROTTLE COMMAND--")
+        s1.write(start_t+str(i)+end_t)
+        #  print("----------- READING -----------")
+        #  print(s1.readline().strip())
+        time.sleep(2)
+    for i in steering_cmds:
+        print("--SENDING STEERING COMMAND--")
+        s2.write(start_s+str(i)+end_s)
+        #  print("----------- READING -----------")
+        #  print(s2.readline().strip())
+        time.sleep(2)
